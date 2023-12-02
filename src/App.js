780ca9803ccb551ad989d2fe5917e2f53653afac
Ayaan Malik <ayaan.malik.in@gmail.com>
2023-12-03 00:03:45 +0530

    major changes

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import NotFound from "./components/NotFound";
 
 function App() {
-
-  return (
-    <>
+  return <>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/About" element={<About/>}/>
         <Route path="/Contact" element={<Contact/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
-    </>
-  );
+  </>;
 }
 
 export default App;
